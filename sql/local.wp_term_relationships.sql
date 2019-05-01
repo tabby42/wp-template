9a0c194f6cff8a56d142c260cf525313ba89f76c
Irene Theiss <irene-rudel@gmx.at>
2019-05-01 23:02:21 +0200

    remove folder

--- sql/local.wp_term_relationships.sql
+++
@@ -1,9 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_term_relationships` VALUES
-(1,1,0),
-(16,2,0),
-(17,2,0),
-(19,3,0),
-(20,3,0);
--- sql/local.wp_users.sql
+++
@@ -1,5 +0,0 @@
-/*!40101 SET NAMES binary*/;
-/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
-/*!40103 SET TIME_ZONE='+00:00' */;
-INSERT INTO `wp_users` VALUES
-(1,"irene","$P$Bt3Q5CpuS1xb6mgceyD1lj/nHPxLMf1","irene","irene.theiss@gmx.at","","2019-04-28 11:14:58","",0,"irene");
