5ed8b0c2071dd7b5404cf1edb29160d5d5577e2e
Irene Theiss <irene-rudel@gmx.at>
2019-05-01 23:51:02 +0200

    add files for theme dev and adjust gitignore

--- settings.js
+++ settings.js
@@ -1,2 +1,5 @@
+// If you're using Local by Flywheel you will
+// want your settings to be similar to the examples below:
+
 exports.themeLocation = './public/wp-content/themes/custom-theme/';
 exports.urlToPreview = 'http://localsite.local/';
